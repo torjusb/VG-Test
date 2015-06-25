7f9e9bd5b971b0efb52df0d3262c65986aa68fea
Torjus Bjåen <torjus.bjaen@gmail.com>
2015-06-25 23:13:08 +0200

    Move varnish log parsing logic to varnishStats service
    Simplifies our controller drastically, and will allow the functionality to be re
    used.
    
    Also ensures the data isn't re-fetched every time the controller is loaded.

--- app/scripts/controllers/varnish.js
+++ app/scripts/controllers/varnish.js
@@ -1,62 +1,15 @@
 'use strict';
 
 angular.module('vgApp')
-    .controller('VarnishCtrl', function ($scope, $http, logParser) {
+    .controller('VarnishCtrl', function ($scope, varnishStats) {
         // Displays loading spinners
         $scope.loadingHosts = $scope.loadingUris = true;
 
-        $http.get('http://crossorigin.me/http://tech.vg.no/intervjuoppgave/varnish.log', {
-            transformResponse: function (data) {
-                // Each request in the log is separated by a new line,
-                // split on it, to get an array of all the requests.
-                var rows = data.trim().split("\n");
+        varnishStats.getTopHosts().then(function (hosts) {
+            $scope.hosts = hosts;
+        });
 
-                // Parse each row. This will give us an array of objects back.
-                return rows.map(logParser.parseRow);
-            }
-        }).then(function (response) {
-            // Will contain data about how many requests each host name has
-            // had. The key is the host name, and the value is number of hits.
-            var hostMap = {};
-            var uriMap = {};
-
-            angular.forEach(response.data, function (request) {
-                // Add the hostname to the `hostMap` if it isn't added yet,
-                // and keep incrementing it's counter every time we see it.
-                if (typeof hostMap[request.host] !== 'undefined') {
-                    hostMap[request.host]++;
-                } else {
-                    hostMap[request.host] = 1;
-                }
-
-                // Do the same process with the URIs
-                if (typeof uriMap[request.uri] !== 'undefined') {
-                    uriMap[request.uri]++;
-                } else {
-                    uriMap[request.uri] = 1;
-                }
-            });
-
-            // Turn the `hostMap` object into an array of objects. This will
-            // allow us to easily sort the items with `orderBy`. `orderBy` will
-            // not work on plain objects.
-            $scope.hostMap = [];
-            angular.forEach(hostMap, function (hits, host) {
-                $scope.hostMap.push({
-                    host: host,
-                    hits: hits
-                });
-            });
-            $scope.loadingHosts = false;
-
-            // Do the same process for the `uriMap`.
-            $scope.uriMap = [];
-            angular.forEach(uriMap, function (hits, uri) {
-                $scope.uriMap.push({
-                    uri: uri,
-                    hits: hits
-                });
-            });
-            $scope.loadingUris = false;
+        varnishStats.getTopUris().then(function (uris) {
+            $scope.uris = uris;
         });
     });
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -31,4 +31,79 @@ angular
                 return parsed;
             }
         };
+    })
+    .factory('varnishStats', function ($http, $q, logParser) {
+        // This will contain all the data, once the HTTP request has finished
+        // and the data is parsed.
+        // Both arrays will contain objects. One object per host name / URI
+        // respectively.
+        var data = {
+            topHosts: [], // `{host: 'vg.no', hits: 3}`
+            topUris: [] // `{uri: '/foo', hits: 96}`
+        };
+
+        // Store the promise in a variable, so we know when the data is ready.
+        var dataReady = $http.get('http://crossorigin.me/http://tech.vg.no/intervjuoppgave/varnish.log', {
+            transformResponse: function (data) {
+                // Each request in the log is separated by a new line,
+                // split on it, to get an array of all the requests.
+                var rows = data.trim().split("\n");
+
+                // Parse each row. This will give us an array of objects back.
+                return rows.map(logParser.parseRow);
+            }
+        }).then(function (response) {
+            // Will contain data about how many requests each host name has
+            // had. The key is the host name, and the value is number of hits.
+            var hostMap = {};
+            var uriMap = {};
+
+            angular.forEach(response.data, function (request) {
+                // Add the hostname to the `hostMap` if it isn't added yet,
+                // and keep incrementing it's counter every time we see it.
+                typeof hostMap[request.host] !== 'undefined' ?
+                    hostMap[request.host]++ : hostMap[request.host] = 1;
+
+                // Do the same process with the URIs
+                typeof uriMap[request.uri] !== 'undefined' ?
+                    uriMap[request.uri]++ : uriMap[request.uri] = 1;
+            });
+
+            // Turn the `hostMap` object into an array of objects. This will
+            // allow us to easily sort the items with `orderBy`. `orderBy` will
+            // not work on plain objects.
+            data.topHosts = [];
+            angular.forEach(hostMap, function (hits, host) {
+                data.topHosts.push({host: host, hits: hits});
+            });
+
+            // Do the same process for the `uriMap`.
+            data.topUris = [];
+            angular.forEach(uriMap, function (hits, uri) {
+                data.topUris.push({uri: uri, hits: hits});
+            });
+        });
+
+        return {
+            getTopHosts: function () {
+                var defer = $q.defer();
+
+                // We return a promise which we resolves once we know the data
+                // from Varnish is loaded.
+                dataReady.then(function () {
+                    defer.resolve(data.topHosts);
+                });
+
+                return defer.promise;
+            },
+            getTopUris: function () {
+                var defer = $q.defer();
+
+                dataReady.then(function () {
+                    defer.resolve(data.topUris);
+                });
+
+                return defer.promise;
+            }
+        };
     });
