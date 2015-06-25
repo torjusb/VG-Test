76802c5e522b5635ff2c63599601472b5b359948
Torjus Bjåen <torjus.bjaen@gmail.com>
2015-06-25 20:01:15 +0200

    Implement JSON-feed view
    Very basic view. Simply lists the articles, newest first.
    
    The JSON-feed is not set up with CORS, so I've used http://crossorigin.me/ as a
    CORS proxy to enable fetching the data directly from the client.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,6 +24,10 @@ angular
                 templateUrl: 'views/about.html',
                 controller: 'AboutCtrl'
             })
+            .when('/json', {
+                templateUrl: 'views/json.html',
+                controller: 'JsonCtrl'
+            })
             .otherwise({
                 redirectTo: '/'
             });
---
+++ app/scripts/controllers/json.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('vgApp')
+    .controller('JsonCtrl', function ($scope, $http) {
+        $http.get('http://crossorigin.me/http://rexxars.com/playground/testfeed/', {
+            headers: {
+                'Accept': 'application/json'
+            },
+            transformResponse: function (data) {
+                data = angular.fromJson(data);
+                angular.forEach(data, function (item) {
+                    item.dateTime = new Date(item.date + ' ' + item.time);
+                });
+
+                return data;
+            }
+        }).then(function (response) {
+            $scope.articles = response.data;
+        });
+    });
